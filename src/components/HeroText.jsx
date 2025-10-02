d2b499bffb3450d81067a03f2007466f13e1f97f
Atmaj Mishra <mishraatmaj@gmail.com>
2025-10-02 12:42:47 +0530

    fixes on dusshera

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -1,7 +1,13 @@
 import { FlipWords } from "./FlipWords";
 import {motion} from "motion/react";
-const words=["Secure","Modern","Scalable"]
+
+
 const HeroText = () => {
+  const words=["Secure","Modern","Scalable"];
+  const variants={
+    hidden:{opacity:0, x:-50},
+    visible:{opacity:1, x:0},
+  }
   return (
     <div
       className="z-10 mt-20 text-center md:mt-40 md:text-left
@@ -9,9 +15,17 @@ const HeroText = () => {
     >
       {/* Desktop View */}
       <div className="flex-col hidden md:flex c-space">
-        <motion.h1 className="text-4xl font-medium">Hi I'm Atmaj</motion.h1>
+        <motion.h1 className="text-4xl font-medium"
+        variants={variants}
+        initial="hidden"
+        animate="visible"
+        transition={{delay:1}}>Hi I'm Atmaj</motion.h1>
         <div className="flex flex-col items-start">
-            <motion.p className="text-5xl font-medium text-neutral-300">
+            <motion.p className="text-5xl font-medium text-neutral-300"
+             variants={variants}
+        initial="hidden"
+        animate="visible"
+        transition={{delay:1.2}}>
             A Developer <br/> Dedicated to Crafting
 
             </motion.p>
