dfd8f749d25d2e552dcea173fd372c3f935186c5
Atmaj Mishra <mishraatmaj@gmail.com>
2025-10-02 11:28:29 +0530

    Almost done with the hero section

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -1,3 +1,6 @@
+import { FlipWords } from "./FlipWords";
+import {motion} from "motion/react";
+const words=["Secure","Modern","Scalable"]
 const HeroText = () => {
   return (
     <div
@@ -6,9 +9,31 @@ const HeroText = () => {
     >
       {/* Desktop View */}
       <div className="flex-col hidden md:flex c-space">
-        <h1>Hi I'm Atmaj</h1>
+        <motion.h1 className="text-4xl font-medium">Hi I'm Atmaj</motion.h1>
+        <div className="flex flex-col items-start">
+            <motion.p className="text-5xl font-medium text-neutral-300">
+            A Developer <br/> Dedicated to Crafting
+
+            </motion.p>
+            <motion.div><FlipWords words={words}
+            className="font-black text-white text-8xl"/></motion.div>
+            <motion.p className="text-4xl font-medium text-neutral-300">Web Solutions</motion.p>
+        </div>
       </div>
-      {/* {Mobile View} */}
+      {/* {Mobile View} */}    
+      <div className="flex-col space-y-6 md:hidden">
+        <motion.p className="text-4xl font-medium">Hi, I'm Atmaj</motion.p>
+        <div>
+          <motion.p className="texl-5xl font-black text-neutral-300">
+            A Developer <br />
+ Dedicated to Crafting          </motion.p>
+ <motion.div>
+  <FlipWords words={words}
+  className="font-black text-white text-7xl"/>
+ </motion.div>
+ <motion.p className="text-4xl font-medium text-neutral-300">Web Solutions</motion.p>
+        </div>
+      </div>
     </div>
   );
 };
