fea61b975e4707dd1ed826d2666019f53a1fa775
Atmaj Mishra <mishraatmaj@gmail.com>
2025-10-05 04:38:31 +0530

    completed the Astronaut Component

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,10 +2,16 @@ import { section } from "motion/react-client";
 import HeroText from "../components/HeroText";
 import ParallaxBackground from "../components/ParallaxBackground";
 import { Astronaut } from "../components/Astronaut";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Float, Loader } from "@react-three/drei";
+import { Suspense } from "react";
+
 import {Flower} from "../components/Flower";
+import { useMediaQuery } from "react-responsive";
+import { easing } from "maath";
+
 const Hero = () => {
+    const isMobile=useMediaQuery({maxWidth:853});
     return (
         <section className="flex items-start justify-center 
         md:items-start md:justify-start min-h-screen overflow-hidden
@@ -14,13 +20,28 @@ const Hero = () => {
             <HeroText/>
             <ParallaxBackground/>
             <figure className="absolute inset-0" style={{width:"100vw",height:"100vh"}} >
-                <Canvas>
-                    <Astronaut/>
-                    <OrbitControls/>
+                <Canvas camera={{position:[0,1,3]}}>
+                    <Suspense >
+
+                    <Float>
+
+                    <Astronaut scale={isMobile && 0.13} position={isMobile && [0,-1.5,0]}/>
+                    </Float>
+                     <Rig/>
+                    </Suspense>
+                    
+                   
                 </Canvas>
             </figure>
          </section>
     );
 };
+function Rig(){
+    return useFrame((state,delta )=>{
+        easing.damp3(state.camera.position,[
+            state.mouse.x/10, 1+state.mouse.y/10,3
+        ],0.5,delta)
+    })
+}
 
 export default Hero;
