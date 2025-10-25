59b5f09e633d9cb4c4678ddf5883ec9448ff965b
Atmaj Mishra <mishraatmaj@gmail.com>
2025-10-25 15:39:53 +0530

    completed project and experience section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from './sections/Navbar'
 import Hero from './sections/Hero'
 import About from './sections/About'
+import Projects from './sections/Projects'
+import Experiences from './sections/Experiences'
 const App = () => {
   return (
     <div className='container mx-auto max-w-7xl '>
@@ -9,7 +11,8 @@ const App = () => {
       <Navbar/>
       <Hero/>
       <About/>
-
+      <Projects/>
+      <Experiences/>
 
 {/* <section  className='min-h-screen'/>
 <section  className='min-h-screen'/>
