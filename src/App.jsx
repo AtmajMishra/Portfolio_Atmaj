44f8103df7774b5c9a1ba99c206bfd052e7b11ad
Atmaj Mishra <mishraatmaj@gmail.com>
2025-10-25 17:50:17 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Hero from './sections/Hero'
 import About from './sections/About'
 import Projects from './sections/Projects'
 import Experiences from './sections/Experiences'
+import Testimonial from './sections/Testimonial'
+import Contact from './sections/Contact'
+import Footer from './sections/Footer'
 const App = () => {
   return (
     <div className='container mx-auto max-w-7xl '>
@@ -13,6 +16,9 @@ const App = () => {
       <About/>
       <Projects/>
       <Experiences/>
+      <Testimonial/>
+      <Contact/>
+      <Footer/>
 
 {/* <section  className='min-h-screen'/>
 <section  className='min-h-screen'/>
